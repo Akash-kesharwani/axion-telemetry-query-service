e65777b19586706631c689c578fdbc47a7781f27
akash.keshari <akash.keshari@hasanbhaikifaujkeraygimoujo.onmicrosoft.com>
2026-09-14 17:09:20 +0530

    update workflow

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Settings:
     # Format: postgresql://<user>:<password>@<host>:<port>/<database>
     DATABASE_URL: str = os.getenv(
         "DATABASE_URL",
-        "postgresql://admin:P@ssw01rd@123@postgres-ser:5432/axion_db",
+        "postgresql://admin:P@ssw01rd@123@postgres-ser:5432/axiondb",
     )
     # Allows configuring a different port, e.g., if we run multiple services
     PORT: int = int(os.getenv("PORT", "8000"))
